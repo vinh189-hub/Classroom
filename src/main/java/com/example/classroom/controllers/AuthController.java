671eff2e26ea626a7aa42766a87e8667be519be1
vinh <vinden687@gmail.com>
2023-02-18 09:52:15 +0700

    a

--- src/main/java/com/example/classroom/controllers/AuthController.java
+++ src/main/java/com/example/classroom/controllers/AuthController.java
@@ -1,9 +1,16 @@
 package com.example.classroom.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
+
+    @GetMapping("/hello")
+    ResponseEntity<String> hello(){
+        return new ResponseEntity<>("Hello World", HttpStatus.valueOf("200"));
+    }
 
 }
