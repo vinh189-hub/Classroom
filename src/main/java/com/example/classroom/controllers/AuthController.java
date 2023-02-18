815c7002c8fc5ca8b7adf3377fa697ef58fffcb1
vinh <vinden687@gmail.com>
2023-02-18 09:09:20 +0700

    init

---
+++ src/main/java/com/example/classroom/controllers/AuthController.java
@@ -0,0 +1,9 @@
+package com.example.classroom.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+}
---
+++ src/test/java/com/example/classroom/ClassroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
